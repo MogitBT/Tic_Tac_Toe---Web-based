ab125f369f3c2a9b07fee23ab717c6292967b9b5
Mogit B T <btmogit@gmail.com>
2024-06-19 22:55:49 +0530

    "Refactored Tic-Tac-Toe game logic, added bot move logic, and updated templates for single and double player modes."

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import random
-import os
-import pickle
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 class Game:
     structure = ["_" for _ in range(9)]
-    current_player = 'X'  # Track the current player
+    current_player = 'X'
 
     @staticmethod
     def layout():
@@ -42,10 +40,25 @@ class Game:
     @staticmethod
     def refresh():
         Game.structure = ["_" for _ in range(9)]
-        Game.current_player = 'X'  # Reset the current player to 'X'
+        Game.current_player = 'X'  
+
+    @staticmethod
+    def update(position, player):
+        if Game.user_input(position, player):
+            if Game.check(player):
+                Game.refresh()
+                return f'Player {player} won!'
+            elif Game.draw():
+                Game.refresh()
+                return 'Match Draw!'
+            else:
+                Game.current_player = 'O' if player == 'X' else 'X'
+                return ''
+        else:
+            return 'Invalid move'
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def home():
     return render_template('home.html')
 
@@ -65,27 +78,14 @@ def double_player():
 
 @app.route('/single_player')
 def single_player():
-    return render_template('single_player.html', game = Game.layout(), current_player = Game.current_player)
+    return render_template('single_player.html', game=Game.layout(), current_player=Game.current_player)
 
 @app.route('/doubleplayer_move', methods=['POST'])
 def doubleplayer_move():
     player = request.form['player']
     position = request.form['position']
 
-    valid_move = Game.user_input(position, player)
-
-    if valid_move:
-        if Game.check(player):
-            message = f'Player {player} won!'
-            Game.refresh()
-        elif Game.draw():
-            message = 'Match Draw!'
-            Game.refresh()
-        else:
-            message = ''
-            Game.current_player = 'O' if player == 'X' else 'X'
-    else:
-        message = 'Invalid move'
+    message = Game.update(position, player)
 
     return render_template('double_player.html', game=Game.layout(), current_player=Game.current_player, message=message)
 
@@ -94,37 +94,73 @@ def singleplayer_move():
     player = request.form['player']
     position = request.form['position']
 
-    valid_move = Game.user_input(position, player)
+    message = Game.update(position, player)
 
-    if valid_move:
-        if Game.check(player):
-            message = f'Player {player} won!'
-            Game.refresh()
-        elif Game.draw():
-            message = 'Match Draw!'
-            Game.refresh()
-        else:
-            message = ''
-            Game.current_player = 'O' if player == 'X' else 'X'
-    else:
-        message = 'Invalid move'
+    if Game.current_player == 'O' and not message:
+        message = bot_move_logic()
 
     return render_template('single_player.html', game=Game.layout(), current_player=Game.current_player, message=message)
 
-@app.route('/refresh', methods =['POST', 'GET'])
-def refresh():
-    if request.method == 'POST':
-        source = request.form.get('source')
-    else:
-        source = request.args.get('source')
+def bot_move_logic():
+    player = 'O'
+    combinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    position = -1
 
+    # Check for winning move
+    for combination in combinations:
+        bot_count = 0
+        empty_spot = -1
+        for i in combination:
+            if Game.structure[i] == 'O':
+                bot_count += 1
+            elif Game.structure[i] == '_':
+                empty_spot = i
+        if bot_count == 2 and empty_spot != -1:
+            position = empty_spot
+            break
+
+    # Block opponent's winning move
+    if position == -1:
+        for combination in combinations:
+            player_count = 0
+            empty_spot = -1
+            for i in combination:
+                if Game.structure[i] == 'X':
+                    player_count += 1
+                elif Game.structure[i] == '_':
+                    empty_spot = i
+            if player_count == 2 and empty_spot != -1:
+                position = empty_spot
+                break
+
+    # Take the center spot if available
+    if position == -1 and Game.structure[4] == '_':
+        position = 4
+
+    # Take any available spot
+    if position == -1:
+        empty_positions = [i for i in range(9) if Game.structure[i] == '_']
+        if empty_positions:
+            position = random.choice(empty_positions)
+
+    position = str(position + 1)
+    message = Game.update(position, 'O')
+
+    return message
+
+@app.route('/refresh', methods=['POST'])
+def refresh():
+    source = request.form.get('source')
+    Game.refresh()
     if source == 'single_player':
-        Game.refresh()
         return redirect(url_for('single_player'))
     else:
-        Game.refresh()
         return redirect(url_for('double_player'))
-        
+
 
 if __name__ == '__main__':
     app.run(debug=True)

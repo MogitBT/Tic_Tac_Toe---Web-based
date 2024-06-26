b87e9cdfc36e7760deb54600c30fff46f3a074c9
Mogit B T <btmogit@gmail.com>
2024-06-26 16:24:33 +0530

    "Refactor Game logic, update templates for home and tutorial pages, and modify routes and functions in app.py"

--- app.py
+++ app.py
@@ -1,64 +1,9 @@
-import random
 from flask import Flask, render_template, request, redirect, url_for
+from game import Game
 
 app = Flask(__name__)
 
-class Game:
-    structure = ["_" for _ in range(9)]
-    current_player = 'X'
-
-    @staticmethod
-    def layout():
-        return [Game.structure[i:i+3] for i in range(0, 9, 3)]
-
-    @staticmethod
-    def user_input(position, player):
-        if position.isdigit() and int(position) in range(1, 10):
-            if Game.structure[int(position) - 1] == "_":
-                Game.structure[int(position) - 1] = player
-                return True
-            else:
-                return False
-        return False
-
-    @staticmethod
-    def check(player):
-        combinations = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8],
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],
-            [0, 4, 8], [2, 4, 6]
-        ]
-        for possibility in combinations:
-            if all(Game.structure[i] == player for i in possibility):
-                return True
-        return False
-
-    @staticmethod
-    def draw():
-        return "_" not in Game.structure
-
-    @staticmethod
-    def refresh():
-        Game.structure = ["_" for _ in range(9)]
-        Game.current_player = 'X'  
-
-    @staticmethod
-    def update(position, player):
-        if Game.user_input(position, player):
-            if Game.check(player):
-                Game.refresh()
-                return f'Player {player} won!'
-            elif Game.draw():
-                Game.refresh()
-                return 'Match Draw!'
-            else:
-                Game.current_player = 'O' if player == 'X' else 'X'
-                return ''
-        else:
-            return 'Invalid move'
-
-
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
 
@@ -82,7 +27,9 @@ def single_player():
 
 @app.route('/tutorial')
 def tutorial():
-    return render_template('tutorial.html')
+    Game.refresh()
+    Game.tutorial_logic()
+    return render_template('tutorial.html', game=Game.layout(), current_player=Game.current_player)
 
 @app.route('/doubleplayer_move', methods=['POST'])
 def doubleplayer_move():
@@ -101,60 +48,17 @@ def singleplayer_move():
     message = Game.update(position, player)
 
     if Game.current_player == 'O' and not message:
-        message = bot_move_logic()
+        message = Game.bot_move_logic()
 
     return render_template('single_player.html', game=Game.layout(), current_player=Game.current_player, message=message)
 
-def bot_move_logic():
-    player = 'O'
-    combinations = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ]
-    position = -1
+@app.route('/tutorial_move', methods=['POST'])
+def tutorial_move():
+    player = request.form['player']
+    position = request.form['position']
 
-    # Check for winning move
-    for combination in combinations:
-        bot_count = 0
-        empty_spot = -1
-        for i in combination:
-            if Game.structure[i] == 'O':
-                bot_count += 1
-            elif Game.structure[i] == '_':
-                empty_spot = i
-        if bot_count == 2 and empty_spot != -1:
-            position = empty_spot
-            break
-
-    # Block opponent's winning move
-    if position == -1:
-        for combination in combinations:
-            player_count = 0
-            empty_spot = -1
-            for i in combination:
-                if Game.structure[i] == 'X':
-                    player_count += 1
-                elif Game.structure[i] == '_':
-                    empty_spot = i
-            if player_count == 2 and empty_spot != -1:
-                position = empty_spot
-                break
-
-    # Take the center spot if available
-    if position == -1 and Game.structure[4] == '_':
-        position = 4
-
-    # Take any available spot
-    if position == -1:
-        empty_positions = [i for i in range(9) if Game.structure[i] == '_']
-        if empty_positions:
-            position = random.choice(empty_positions)
-
-    position = str(position + 1)
-    message = Game.update(position, 'O')
-
-    return message
+    message = Game.tutorial_update(position)
+    return render_template('tutorial.html', game=Game.layout(), current_player=Game.current_player, message=message)
 
 @app.route('/refresh', methods=['POST'])
 def refresh():
@@ -162,9 +66,10 @@ def refresh():
     Game.refresh()
     if source == 'single_player':
         return redirect(url_for('single_player'))
+    elif source == 'tutorial':
+        return redirect(url_for('tutorial'))
     else:
         return redirect(url_for('double_player'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ game.py
@@ -0,0 +1,145 @@
+import random
+class Game:
+    structure = ["_" for _ in range(9)]
+    current_player = 'X'
+    tutorial_step = 0
+    hint_index = 0
+    tutorial_count = 0
+    combination = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    bot_combination = [
+        [6, 4, 8], [1, 8, 6], [0, 4, 2],
+        [2, 4, 8], [0, 2, 8], [0, 4, 6],
+        [1, 6, 4], [1, 3, 7]
+    ]
+
+    def layout():
+        return [Game.structure[i:i+3] for i in range(0, 9, 3)]
+
+    def user_input(position, player):
+        if position.isdigit() and int(position) in range(1, 10):
+            if Game.structure[int(position) - 1] == "_":
+                Game.structure[int(position) - 1] = player
+                return True
+            else:
+                return False
+        return False
+
+    def check(player):
+        for possibility in Game.combination:
+            if all(Game.structure[i] == player for i in possibility):
+                return True
+        return False
+
+    def draw():
+        if "_" not in Game.structure:
+            return True
+
+    def refresh():
+        Game.structure = ["_" for _ in range(9)]
+        Game.current_player = 'X'
+        Game.tutorial_step = 0
+        Game.hint_index = 0
+
+    def update(position, player):
+        if Game.user_input(position, player):
+            if Game.check(player):
+                Game.refresh()
+                return f'Player {player} won!'
+            elif Game.draw():
+                Game.refresh()
+                return 'Match Draw!'
+            else:
+                Game.current_player = 'O' if player == 'X' else 'X'
+                return ''
+        else:
+            return 'Invalid move'
+        
+    def bot_move_logic():
+        combinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ]
+        position = -1
+
+        for combination in combinations:
+            bot_count = 0
+            empty_spot = -1
+            for i in combination:
+                if Game.structure[i] == 'O':
+                    bot_count += 1
+                elif Game.structure[i] == '_':
+                    empty_spot = i
+            if bot_count == 2 and empty_spot != -1:
+                position = empty_spot
+                break
+
+        if position == -1:
+            for combination in combinations:
+                player_count = 0
+                empty_spot = -1
+                for i in combination:
+                    if Game.structure[i] == 'X':
+                        player_count += 1
+                    elif Game.structure[i] == '_':
+                        empty_spot = i
+                if player_count == 2 and empty_spot != -1:
+                    position = empty_spot
+                    break
+
+        if position == -1 and Game.structure[4] == '_':
+            position = 4
+
+        if position == -1:
+            empty_positions = [i for i in range(9) if Game.structure[i] == '_']
+            if empty_positions:
+                position = random.choice(empty_positions)
+
+        position = str(position + 1)
+        message = Game.update(position, 'O')
+
+        return message
+
+    def tutorial_update(position):
+        if position.isdigit() and int(position) in range(1, 10):
+            pos_index = int(position) - 1
+            if Game.tutorial_step <= len(Game.combination):
+                expected_positions = Game.combination[Game.tutorial_count]
+                if pos_index == expected_positions[Game.hint_index]:
+                    Game.structure[pos_index] = "X"
+                    Game.hint_index += 1
+                    if Game.hint_index == len(expected_positions):
+                        Game.tutorial_step += 1
+                        Game.hint_index = 0
+                        if Game.tutorial_step < len(Game.combination):
+                            Game.refresh()
+                            Game.tutorial_count += 1
+                            tutorial_logic()
+                            return "Well done! Proceeded to the next level."
+                        else:
+                            return "Tutorial complete! You've learned all the steps."
+                    bot_positions = Game.bot_combination[Game.tutorial_step]
+                    for bot_pos in bot_positions:
+                        if Game.structure[bot_pos] == '_':
+                            Game.structure[bot_pos] = "O"
+                            break
+                    tutorial_logic()  
+                    return "Correct move! Now it's your turn again."
+                else:
+                    return "Incorrect position. Try again."
+            else:
+                return "Tutorial complete! You've learned all the steps."
+        else:
+            return "Invalid input. Please enter a number between 1 and 9."
+
+    def tutorial_logic():
+        if Game.tutorial_step <= len(Game.combination):
+            next_hint_position = Game.combination[Game.tutorial_count][Game.hint_index]
+            if Game.structure[next_hint_position] == "_":
+                Game.structure[next_hint_position] = "√"
+        else:
+            Game.refresh()
